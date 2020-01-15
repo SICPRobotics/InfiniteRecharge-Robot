a296b717fe1957b9ef2d0911245fb8f5fc297e27
Samuel Lynas <sjtl2004@gmail.com>
2020-01-15 16:36:55 -0600

    Working on Ground Intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -66,9 +66,10 @@ public class RobotContainer {
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
-   */
+   *
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return autoCommand;
   }
+  */
 }
--- src/main/java/frc/robot/subsystems/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/GroundIntake.java
@@ -1,13 +1,20 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class GroundIntake extends SubsystemBase {
-    public GroundIntake() {
 
+    private VictorSP intakeMotor;
+    public GroundIntake() {
+        intakeMotor = new VictorSP(4);
     }
 
-    public void run() {
-        
+    public void start() {
+        intakeMotor.set(.5);
+    }
+
+    public void stop() {
+        intakeMotor.set(0);
     }
 }
