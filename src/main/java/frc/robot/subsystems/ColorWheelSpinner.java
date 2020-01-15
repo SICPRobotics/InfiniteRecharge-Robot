1e5e1add45ea88d1ab9f48ddf7d12bbc70da5f2c
unknown <ignatiusrobotics@gmail.com>
2020-01-15 16:21:57 -0600

    Basic color wheel subsystem + command

--- src/main/java/frc/robot/subsystems/ColorWheelSpinner.java
+++ src/main/java/frc/robot/subsystems/ColorWheelSpinner.java
@@ -2,56 +2,64 @@ package frc.robot.subsystems;
 
 import java.util.Arrays;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.game_elements.ColorWheel;
 import frc.robot.game_elements.ColorWheelColor;
 
 public class ColorWheelSpinner extends SubsystemBase {
-
+    VictorSP spinnerMotor = new VictorSP(4);
+    private final ColorMatch colorMatcher = new ColorMatch();
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
     
-
-    
-    
-    
-    public void spinTo(ColorWheelColor fromColor, ColorWheelColor toColor) {
-        
-        ColorWheel colorwheel = new ColorWheel();
-        Timer spinnerTimer = new Timer();
-        int directionOfSpin = 1;
-        if(colorwheel.distanceTo(fromColor, toColor) < 0)
-        {
-            directionOfSpin = -1;
-        }
-        else if(colorwheel.distanceTo(fromColor, toColor) > 0)
-        {
-            directionOfSpin = 1;
-        }
-        else 
-        {
-            directionOfSpin = 0;
-        }
-        spinnerTimer.start();
-        if(spinnerTimer.get() < 0.2)
-        {
-            spinnerMotor.set(0.75 * directionOfSpin);
-        }
-        spinnerTimer.reset();
-
+    public ColorWheelSpinner() {
+        Arrays.stream(ColorWheelColor.values()).forEach(colorWheelColor -> {
+            colorMatcher.addColorMatch(colorWheelColor.targetColor);
+        });
     }
 
-    public ColorWheelColor toColor()
-    {
+    /**
+     * Gets the number of slices between the current color and the closest slice of the target color.
+     * Returns a negative number if the closest slice is behind.
+     */
+    public int getSlicesTo(ColorWheelColor targetColor) {
+        return ColorWheelColor.distanceTo(getCurrentColor(), targetColor);
+    }
+
+    /**
+     * Gets the color that FMS wants us to spin to.
+     */
+    public ColorWheelColor getTargetColor() {
         String color = DriverStation.getInstance().getGameSpecificMessage();
         ColorWheelColor nextColor = Arrays.stream(ColorWheelColor.values()).filter(c -> c.string.equals(color)).toArray(ColorWheelColor[]::new)[0];
         return nextColor;
-
-
-
     }
+
+    /**
+     * Gets the color that the color wheel is on right now.
+     * @return the color that the color wheel is on right now.
+     */
+    public ColorWheelColor getCurrentColor() {
+        Color detectedColor = colorSensor.getColor();
+        ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
+        return Arrays.stream(ColorWheelColor.values()).filter(colorWheelColor -> colorWheelColor.targetColor == match.color).toArray(ColorWheelColor[]::new)[0];
+    }
+
+    public void set(double val) {
+        spinnerMotor.set(val);
+    }
+
 
 }
