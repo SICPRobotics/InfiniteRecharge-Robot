a49c9e394f0e2133229a137a67433ee48dcc5dcd
unknown <ignatiusrobotics@gmail.com>
2020-01-16 15:33:14 -0600

    Saved work from last time

---
+++ src/main/java/frc/robot/commands/AutonomusCommand.java
@@ -0,0 +1,73 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class AutonomusCommand extends CommandBase {
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+    private final DriveTrain drive;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public AutonomusCommand(DriveTrain subsystem) {
+    drive = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override 
+  public void initialize() {
+    
+}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Timer timer = new Timer();
+
+    timer.start();
+    if(timer.get() < 3.0)
+    {
+        drive.cheesyDrive(1.0, 0.0);
+    }
+    else
+    {
+        drive.cheesyDrive(0.0, -1.0);
+        timer.reset();
+        isFinished();
+    }
+
+    
+
+
+
+}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
