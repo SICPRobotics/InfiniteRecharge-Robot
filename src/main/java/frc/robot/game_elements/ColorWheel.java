5280764f1067659ff71d05eee7cf3bacac738356
SploxFox <sjtl2004@gmail.com>
2020-01-21 16:49:58 -0600

    These methods should actually work now

--- src/main/java/frc/robot/game_elements/ColorWheel.java
+++ src/main/java/frc/robot/game_elements/ColorWheel.java
@@ -13,15 +13,55 @@ public class ColorWheel {
         ColorWheelColor.GREEN
     }));
 
+    /*
+
     private static int distanceToWithoutWrapping(ColorWheelColor fromColor, ColorWheelColor toColor) {
         return ColorWheel.order.indexOf(toColor) - ColorWheel.order.indexOf(fromColor);
     }
 
     private static int distanceWrappedTo(ColorWheelColor fromColor, ColorWheelColor toColor) {
-        return (ColorWheel.order.indexOf(toColor) - ColorWheel.order.size()) - ColorWheel.order.indexOf(toColor);
+        //return ColorWheel.order.indexOf(fromColor) - (ColorWheel.order.indexOf(toColor) - ColorWheel.order.size());
+        return (order.indexOf(toColor) < order.indexOf(fromColor) ? order.indexOf(toColor) - order.size() : order.indexOf(toColor) + order.size()) - order.indexOf(fromColor);
     }
 
-    public static int distanceTo(ColorWheelColor fromColor, ColorWheelColor toColor) {
+    */
+
+    /*public static int distanceTo(ColorWheelColor fromColor, ColorWheelColor toColor) {
         return Math.abs(distanceToWithoutWrapping(fromColor, toColor)) < Math.abs(distanceWrappedTo(fromColor, toColor)) ? distanceToWithoutWrapping(fromColor, toColor) : distanceWrappedTo(fromColor, toColor);
+    }*/
+
+    /**
+     * Finds the number of slices to the closest toColor from the fromColor.
+     * @param fromColor the color we start at
+     * @param toColor the color we're going to
+     * @return a positive (up) or negative (down) number that represents the number of slices from the fromColor
+     * to the toColor
+     */
+    public static int slicesToClosest(ColorWheelColor fromColor, ColorWheelColor toColor) {
+        return Math.min(slicesUp(fromColor, toColor),slicesDown(fromColor, toColor)) * (slicesUp(fromColor, toColor) < slicesDown(fromColor, toColor) ? 1 : -1);
+    }
+
+    public static ColorWheelColor getNextColor(ColorWheelColor currentColor) {
+        return order.get((order.indexOf(currentColor) + 1) % order.size());
+    }
+
+    /**
+     * Finds the number of slices away a color is one direction on the wheel
+     * @param fromColor the color we want to start counting from
+     * @param toColor the color we want to stop counting at
+     * @return the number of slices (a positive number) that the color is away in one direction
+     */
+    public static int slicesUp(ColorWheelColor fromColor, ColorWheelColor toColor) {
+        return order.indexOf(fromColor) < order.indexOf(toColor) ? order.indexOf(toColor) - order.indexOf(fromColor) : order.size() + order.indexOf(toColor) - order.indexOf(fromColor);
+    }
+
+    /**
+     * Finds the number of slices away a color is one direction on the wheel
+     * @param fromColor the color we want to start counting from
+     * @param toColor the color we want to stop counting at
+     * @return the number of slices (a positive number) that the color is away in one direction
+     */
+    public static int slicesDown(ColorWheelColor fromColor, ColorWheelColor toColor) {
+        return order.indexOf(fromColor) > order.indexOf(toColor) ? order.indexOf(fromColor) - order.indexOf(toColor) : order.indexOf(fromColor) + order.size() - order.indexOf(toColor);
     }
 }
