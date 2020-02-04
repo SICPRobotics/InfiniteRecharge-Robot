4c92d732da91a6f10a078f606f325d7e350aa2b6
SploxFox <sjtl2004@gmail.com>
2020-02-04 16:47:54 -0600

    Merged master back into this

---
+++ src/main/java/frc/robot/controllers/Stick.java
@@ -0,0 +1,17 @@
+package frc.robot.controllers;
+
+import java.util.function.DoubleSupplier;
+
+public class Stick {
+    private DoubleSupplier getXSupplier, getYSupplier;
+    public Stick(DoubleSupplier getX, DoubleSupplier getY) {
+        this.getXSupplier = getX;
+        this.getYSupplier = getY;
+    }
+    public double getX() {
+        return this.getXSupplier.getAsDouble();
+    }
+    public double getY() {
+        return this.getYSupplier.getAsDouble();
+    }
+}
