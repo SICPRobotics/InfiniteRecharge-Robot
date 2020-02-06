01b571b7156021af963e7045b571388792186035
SploxFox <sjtl2004@gmail.com>
2020-02-06 17:02:03 -0600

    Now implements PistonSubsystem

--- src/main/java/frc/robot/subsystems/Gate.java
+++ src/main/java/frc/robot/subsystems/Gate.java
@@ -5,18 +5,17 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public final class Gate extends SubsystemBase {
+public final class Gate extends SubsystemBase implements PistonSubsystem {
     private DoubleSolenoid solenoid;
     public Gate() {
         solenoid = new DoubleSolenoid(Constants.Gate.FORWARD_SOLENOID_ID, Constants.Gate.REVERSE_SOLENOID_ID);
     }
 
-    public void extend()
-    {
+    public void pistonForward() {
         solenoid.set(Value.kForward);
     }
 
-    public void retract() {
+    public void pistonReverse() {
         solenoid.set(Value.kReverse);
     }
 }
