697c7f14cadbb5e8c17c009e7cc2d8105927b5b0
SploxFox <sjtl2004@gmail.com>
2020-02-07 10:08:53 -0600

    Style updates

--- src/main/java/frc/robot/subsystems/Compessor.java
+++ src/main/java/frc/robot/subsystems/Compessor.java
@@ -5,12 +5,10 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Compessor extends SubsystemBase
-{
-    Compressor compressor;
+public class Compessor extends SubsystemBase {
+    private final Compressor compressor;
     
-    public Compessor()
-    {
+    public Compessor() {
         compressor = new Compressor(Constants.Compressor.COMPRESSOR_ID);
        
         compressor.setClosedLoopControl(true);
