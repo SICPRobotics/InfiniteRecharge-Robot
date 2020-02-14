e203de445628667cf839e1838760fc3136772fb7
SploxFox <sjtl2004@gmail.com>
2020-02-14 17:35:52 -0600

    Started motor configs

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -12,23 +16,43 @@ import frc.robot.SubsystemBaseWrapper;
  * the DriveTrain, aka the thing that moves the robot
  */
 public final class DriveTrain extends SubsystemBaseWrapper {
-    private final DifferentialDrive robotDrive;
-    private final TalonSRX frontRight, frontLeft;
+    private final TalonSRX frontRight;
+    private final TalonSRX frontLeft;
 
-    // SEE : https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java/VelocityClosedLoop_ArbFeedForward/src/main/java/frc/robot/Robot.java
+    /* SEE : 
+https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java/VelocityClosedLoop_ArbFeedForward/src/main/java/frc/robot/Robot.java
+    */
 
     public DriveTrain() {
         super();
         // Motors
         frontRight = new WPI_TalonSRX(0);
         TalonSRX rearRight = new WPI_TalonSRX(1);
-        rearRight.follow(frontRight);
-        rearRight.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
 
         frontLeft = new WPI_TalonSRX(3);
         TalonSRX rearLeft = new WPI_TalonSRX(2);
+
+        //Disable and wipe them
+        frontRight.set(ControlMode.PercentOutput, 0);
+        rearRight.set(ControlMode.PercentOutput, 0);
+        frontLeft.set(ControlMode.PercentOutput, 0);
+        rearLeft.set(ControlMode.PercentOutput, 0);
+
+        frontRight.configFactoryDefault();
+        rearRight.configFactoryDefault();
+        frontLeft.configFactoryDefault();
+        rearLeft.configFactoryDefault();
+
+        frontRight.setNeutralMode(NeutralMode.Brake);
+        frontLeft.setNeutralMode(NeutralMode.Brake);
+
+        // Followers (back motors)
+        rearRight.follow(frontRight);
         rearLeft.follow(frontLeft);
-        rearLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+        // Masters (front motors)
+        frontLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 30);
+        frontRight.configRemoteFeedbackFilter(frontLeft.getDeviceID(), RemoteSensorSource.TalonSRX_SelectedSensor, 0, 30);
     }
 
     //Mostly taken from last year's robot
