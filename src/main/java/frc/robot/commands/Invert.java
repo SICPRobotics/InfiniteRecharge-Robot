f41450fe9c3bf1b34b084375fe4af64dd1e006c7
SploxFox <sjtl2004@gmail.com>
2020-02-14 15:37:41 -0600

    Revert "Added invert button and variable robot speed on joystick --Will"
    This reverts commit 3b3e97cf4372d796fb92276d0e4b94562bcb2a70.

---
+++ src/main/java/frc/robot/commands/Invert.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+public final class Invert extends CommandBase {
+    private final DriveTrain driveTrain;
+
+    public Invert(final DriveTrain driveTrain) {
+        this.driveTrain = driveTrain;
+    }
+
+    @Override
+    public void initialize() {
+        driveTrain.invert();
+    }
+    @Override
+    public void end(boolean interrupted) {
+        
+    }
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
