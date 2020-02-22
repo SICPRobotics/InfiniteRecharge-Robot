bb33c38ef2b0c1e07d7576c83978252bced9a46d
Will Crosby <>
2020-02-22 12:48:22 -0600

    Switched to PIDController, no longer extends PIDCommand

--- src/main/java/frc/robot/commands/DriveWithJoystickGyro.java
+++ src/main/java/frc/robot/commands/DriveWithJoystickGyro.java
@@ -1,13 +1,29 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleConsumer;
+
 import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.CheesyDriveGetters;
 import frc.robot.subsystems.DriveTrain;
 
-public class DriveWithJoystickGyro extends PIDCommand {
+public class DriveWithJoystickGyro extends CommandBase {
     private final DriveTrain driveTrain;
-    public DriveWithJoystickGyro(final DriveTrain driveTrain) {
-        super(new PIDController(0, 0, 0), driveTrain::getGyroVelocity, 0, m_useOutput, null);
+    private final CheesyDriveGetters cheesyDriveGetters;
+    private final PIDController pidController;
+    public DriveWithJoystickGyro(final DriveTrain driveTrain, final CheesyDriveGetters cheesyDriveGetters) {
+        pidController = new PIDController(0, 0, 0);
+        this.cheesyDriveGetters = cheesyDriveGetters;
         this.driveTrain = driveTrain;
+        addRequirements(driveTrain);
+    }
+
+    @Override
+    public void execute() {
+        driveTrain.cheesyDrive(
+                -this.cheesyDriveGetters.getForward() * (cheesyDriveGetters.getIsInverted() ? -1 : 1),
+                pidController.calculate(driveTrain.getGyroVelocity(), cheesyDriveGetters.getRotate() * 30),
+                this.cheesyDriveGetters.getScale()
+        );
     }
 }
